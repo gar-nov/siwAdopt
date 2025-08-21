f6c477011cc9f9d4a74b92672fc5ef801ec021ce
sofia_ <sofia_@localhost>
2025-08-21 19:22:40 +0200

    Test Commit

--- siwAdopt/src/main/java/it/uniroma3/siw/SiwAdoptApplication.java
+++ siwAdopt/src/main/java/it/uniroma3/siw/SiwAdoptApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SiwAdoptApplication {
 	
+	// test commit
 	public static void main(String[] args) {
 		SpringApplication.run(SiwAdoptApplication.class, args);
 	}
